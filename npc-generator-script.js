2f4a8c1ae485d91719ab244833a6a8f00f4bfb01
Michał Kanios <michalkanios@outlook.com>
2024-03-05 20:55:48 +0100

    Functional NPC Generator
    Will add more flavour later, but for now? It works. I couldn't get a name randomizer to work just yet, BUT, everything else (race, class, background, high stat, dump stat) is fine. The flavour I'll be adding will be personality traits, ideals, bonds - all of which will be influenced by the character's class, background and stats.

---
+++ npc-generator-script.js
@@ -0,0 +1,358 @@
+// const dragonbornFirst = [
+//     "Arjhan",
+//     "Balasar",
+//     "Bharash",
+//     "Donaar",
+//     "Ghesh",
+//     "Heskan",
+//     "Kriv",
+//     "Medrash",
+//     "Mehen",
+//     "Nadarr",
+//     "Pandjed",
+//     "Patrin",
+//     "Rhogar",
+//     "Shamash",
+//     "Shedinn",
+//     "Tarhun",
+//     "Torinn",
+//     "Tever",
+//     "Arsiarth",
+//     "Griddry",
+//     "Xalanthar",
+//     "Axcurios",
+//     "Bazireus",
+//     "Cobalt",
+//     "Rubicon",
+//     "Xronikus",
+//     "Zhriyagh",
+//     "Dovakhin",
+//     "Shever",
+//     "Arrash",
+//     "Rhomash",
+//     "Tolasar",
+//     "Arhun",
+//     "Balaar",
+//     "Shedesh",
+//     "Bazinaar",
+//     "Bezoar",
+//     "Gorskan",
+//     "Kriskan",
+//     "Tebalt",
+//     "Cobicon",
+//     "Dovahen",
+//     "Ghetrin",
+//     "Ghever",
+//     "Toxcurios"
+// ]
+
+// const dragonbornLast = [
+//     "Clethiallor",
+//     "Daardendrian",
+//     "Delmirev",
+//     "Drachedandion",
+//     "Fenkebradon",
+//     "Kepemolik",
+//     "Kerrhylon",
+//     "Kimbatuul",
+//     "Linxandalor",
+//     "Myastan",
+//     "Nemmonis",
+//     "Norixius",
+//     "Ophshtalajiir",
+//     "Prexjandilin",
+//     "Sheseliath",
+//     "Turnuroth",
+//     "Verthurgiesh",
+//     "Yarjerit",
+//     "Kezzayrth",
+//     "Froithudeom",
+//     "Tretryen",
+//     "Frostmaw",
+//     "Oozebog",
+//     "Firefang",
+//     "Hardscale",
+//     "Forktongue",
+//     "Acidborne",
+//     "Toxicus",
+//     "Lightnington",
+//     "Thunderyen",
+//     "Radiantoth",
+//     "Necroticus",
+//     "Lightajiir",
+//     "Darkallor",
+//     "Krematos",
+// ]
+
+// const dwarfFirst = [
+//     "Adrik",
+//     "Baern",
+//     "Bruenor",
+//     "Dain",
+//     "Delg",
+//     "Eberk",
+//     "Einkil",
+//     "Fargrim",
+//     "Kildrak",
+//     "Morgran",
+//     "Orsik",
+//     "Rurik",
+//     "Taklinn",
+//     "Thoradin",
+//     "Thorin",
+//     "Tordek",
+//     "Traubon",
+//     "Ulfgar",
+//     "Veit",
+//     "Vondal",
+//     "Torbjorn",
+//     "Bjorn",
+//     "Axel",
+//     "Olof",
+//     "Grendel",
+//     "Sven",
+//     "Olovik",
+//     "Ghimbus",
+//     "Faenrir",
+//     "Freyror",
+//     "Odin",
+//     "Helmut",
+//     "Anders",
+//     "Freyan",
+//     "Bulghus",
+//     "Trollidar",
+//     "Bob",
+//     "Haesteinn",
+//     "Jarl",
+//     "Ghimli",
+//     "Senshi",
+//     "Ghimbly"
+// ]
+
+// const dwarfLast = [
+// "Balderk",
+//     "Battlehammer",
+//     "Brawnanvil",
+//     "Dankil",
+//     "Fireforge",
+//     "Frostbeard",
+//     "Gorunn",
+//     "Holderhek",
+//     "Ironfist",
+//     "Loderr",
+//     "Lutgehr",
+//     "Rumnaheim",
+//     "Strakeln",
+//     "Torunn",
+//     "Ungart",
+//     "Onyxfury",
+//     "Brewfinger",
+//     "Caskborn",
+//     "Grayheart",
+//     "Trollidarsson",
+//     "Odinsson",
+//     "Axelsson",
+//     "Haesteinnsson",
+//     "Bjornsson",
+//     "Bjorksson",
+//     "Powerwolf",
+//     "Hammerfall",
+//     "Axebreaker",
+//     "Torchholder",
+//     "Lokisson",
+//     "Ghimlisson",
+//     "Svensson",
+//     "Olofsson",
+//     "Andersson",
+//     "Freyrorsson",
+//     "Faenrirsson",
+//     "Fagrimsson",
+//     "Grimbalksson",
+//     "Mogransson",
+//     "Mountainheart"
+// ]
+
+function test() {
+    blurt({
+        title: 'testFunction',
+        text: 'working properly',
+        type: 'info',
+        okButtonText: 'Click anywhere to exit',
+        escapable: true
+    });
+}
+
+const randomRace = [
+    "Human",
+    "Asmodeus Tiefling",
+    "Zariel Tiefling",
+    "Mephisto Tiefling",
+    "Half-Elf",
+    "High Elf",
+    "Wood Elf",
+    "Drow",
+    "Mountain Dwarf",
+    "Hill Dwarf",
+    "Duergar",
+    "Chromatic Dragonborn",
+    "Metallic Dragonborn",
+    "Variant Human",
+    "Lightfoot Halfling",
+    "Stout Halfling",
+    "Half-Orc",
+    "Orc",
+    "Rock Gnome",
+    "Forest Gnome",
+    "Deep Gnome",
+    "Genasi",
+    "Changeling",
+    "Centaur",
+    "Eladrin",
+    "Fairy",
+    "Githyanki/Githzerai",
+    "Goliath",
+    "Tabaxi",
+    "Tortle",
+    "Harengon",
+    "Kobold",
+    "Goblin",
+    "Hobgoblin",
+    "Bugbear",
+    "Aasimar",
+    "Aarakocra",
+    "Lizardfolk",
+    "Yuan-Ti",
+    "Kenku",
+    "Minotaur",
+    "Satyr",
+    "Sea Elf",
+    "Shadar-Kai",
+    "Triton",
+    "Shifter",
+    "Warforged",
+    "Firbolg"
+]
+const randomClass = [
+"Fighter",
+"Wizard",
+"Rogue",
+"Cleric",
+"Paladin",
+"Druid",
+"Ranger",
+"Sorcerer",
+"Barbarian",
+"Warlock",
+"Monk",
+"Bard",
+"Artifcer",
+]
+
+const randomAlignment = [
+    "Lawful Good",
+    "Neutral Good",
+    "Chaotic Good",
+    "Lawful Neutral",
+    "True Neutral",
+    "Chaotic Neutral",
+    "Lawful Evil",
+    "Neutral Evil",
+    "Chaotic Evil",
+]
+
+const randomPast = [
+    "Acolyte",
+    "Sage",
+    "Urchin",
+    "Noble",
+    "Charlatan",
+    "Faceless",
+    "Criminal",
+    "Knight",
+    "Soldier",
+    "Folk Hero",
+    "Gladiator",
+    "Entertainer",
+    "Guild Artisan",
+    "Haunted One",
+    "Pirate",
+    "Sailor",
+    "Hermit",
+    "Outlander",
+    "Anthropologist",
+    "Archeologist"
+]
+
+const randomHigh = [
+    "Strength",
+    "Dexterity",
+    "Constitution",
+    "Intelligence",
+    "Wisdom",
+    "Charisma"
+]
+
+const randomLow = [
+    "Strength",
+    "Dexterity",
+    "Constitution",
+    "Intelligence",
+    "Wisdom",
+    "Charisma"
+]
+
+
+
+    
+
+function rollChar() {
+    selectedName = document.getElementById("charName");
+    selectedRace = document.getElementById("raceSelect").value;
+    selectedClass = document.getElementById("classSelect").value;
+    selectedAlignment = document.getElementById("alignmentSelect").value;
+    selectedPast = document.getElementById("pastSelect").value;
+    selectedHigh = document.getElementById("highSelect").value;
+    selectedLow = document.getElementById("lowSelect").value;
+    
+        const randomRaceIndex = Math.floor(Math.random() * randomRace.length)
+        const randomClassIndex = Math.floor(Math.random() * randomClass.length)
+        const randomAlignmentIndex = Math.floor(Math.random() * randomAlignment.length)
+        const randomPastIndex = Math.floor(Math.random() * randomPast.length)
+        const randomHighIndex = Math.floor(Math.random() * randomHigh.length)
+        const randomLowIndex = Math.floor(Math.random() * randomLow.length)
+
+        if(selectedRace == "Random"){
+            selectedRace = randomRace[randomRaceIndex];
+        }
+        if(selectedClass == "Random"){
+            selectedClass = randomClass[randomClassIndex];
+        }
+        if(selectedAlignment == "Random"){
+            selectedAlignment = randomAlignment[randomAlignmentIndex];
+        }
+        if(selectedPast == "Random"){
+            selectedPast = randomPast[randomPastIndex];
+        }
+        if(selectedHigh == "Random"){
+            selectedHigh = randomHigh[randomHighIndex];
+        }
+        if(selectedLow == "Random"){
+            selectedLow = randomLow[randomLowIndex];
+        }
+
+        if(selectedLow == selectedHigh){
+            randomHighIndex = Math.floor(Math.random() * randomHigh.length)
+            randomLowIndex = Math.floor(Math.random() * randomLow.length)
+            selectedHigh = randomHigh[randomHighIndex];
+            selectedLow = randomLow[randomLowIndex];
+        }
+
+        blurt({
+            title: 'Your NPC:',
+            text: `${selectedPast} and a ${selectedAlignment} ${selectedRace} ${selectedClass}, whose highest statistic is ${selectedHigh} and lowest is ${selectedLow}.`,
+            type: 'warning',
+            okButtonText: 'Click anywhere to exit',
+            escapable: true
+        });
+    
+}
